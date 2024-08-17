8a133ed2acb62ca8cedd8a983e3f0012c8b0288a
Pratik Singh <pratikkumars623@gmail.com>
2024-08-18 00:08:59 +0530

    Updated

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+
+
+export const HERO_CONTENT = `A Passionate Full Stack Web Developer from India.Currently in 3rd year at Heritage Intite of Technology, Kolkata.I am currently exlploring Backend development using Javascript.Eager to contribute in ever-evolving Technology.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 1 years of experience, I have worked with a variety of technologies, including React, Tailwind CSS, Flask, Javascript, MySQL, and SQlite. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Software Engineering Fellow",
+    company: "HeadStarterAI",
+    description: `Working as a Full Stack Developer in Track-B( Final project that takes a startup’s current backlog and builds it 
+meeting business requirements.)
+`,
+    technologies: ["Javascript", "React.js", "Tailwind CSS", "Flask","SQLite"],
+  },
+  // {
+  //   year: "2022 - 2023",
+  //   role: "Frontend Developer",
+  //   company: "Adobe",
+  //   description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+  //   technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  // },
+  // {
+  //   year: "2021 - 2022",
+  //   role: "Full Stack Developer",
+  //   company: "Facebook",
+  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  // },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
+];
+
+export const PROJECTS = [
+  {
+    title: "IndustryXpert",
+    image: project1,
+    description:
+      "Industry Xpert is an AI based cutting-edge web application aimed at revolutionizing safety protocols within industrial environments. I have developed the entire Front-end of this project for user friendly interface.Also I handled major Backend Portions(50-55%)",
+    technologies: ["HTML", "CSS", "Bootstrap", "Flask", "SQLAlchemy","Javascript"],
+  },
+  {
+    title: "MediConnect",
+    image: project2,
+    description:
+      "MediConnect provides comprehensive 24/7 healthcare services, allowing you to manage your health anytime, anywhere. This application supports early detection and proper medication, crucial for conditions like Parkinson's disease.",
+    technologies: ["HTML", "CSS", "Tailwind CSS", "Flask","Javascript","SQLite"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["Tailwind CSS", "Javascript", "React", "Framer Motion"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kolkata,Anandapur-700107 ",
+  phoneNo: "+91 7596986346",
+  email: "pratiks.tech@gmail.com",
+};
