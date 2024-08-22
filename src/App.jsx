46c8fd7f63191a3737e6996ad203bb1b9a65462c
Pratik Singh <pratikkumars623@gmail.com>
2024-08-23 01:17:57 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/about'
@@ -10,22 +9,22 @@ import Contact from './components/Contact'
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full"> 
-      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div></div>
-      </div>
+        <div className="fixed top-0 -z-10 h-full w-full">
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        </div>
      
       <div className='container mx-auto px-8'>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Technologies/>
-      <Experience/>
-      <Projects/>
-      <Contact/>  
-
+          <Navbar/>
+          <Hero/>
+          <About/>
+          <Technologies/>
+          <Experience/>
+          <Projects/>
+          <Contact/>  
       </div>
       </div>
      
+      // <div className="relative h-full w-full bg-slate-950">
      
   
   )
