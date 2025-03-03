4068ba3f572ca0e9241cc2fd4e060376d8230cef
Pratik Singh <pratikkumars623@gmail.com>
2025-03-04 01:09:21 +0530

    Portfolio v2 final push

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
         <div className="fixed top-0 -z-10 h-full w-full">
-            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div></div>
         </div>
      
       <div className='container mx-auto px-8'>
